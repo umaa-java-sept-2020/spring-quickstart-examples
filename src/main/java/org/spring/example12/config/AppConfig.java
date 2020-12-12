8ee400d251357a299cdf84d8b3f871377fc14fd8
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-12-12 16:47:29 +0530

    added property file & comments

--- src/main/java/org/spring/example12/config/AppConfig.java
+++ src/main/java/org/spring/example12/config/AppConfig.java
@@ -3,7 +3,7 @@ package org.spring.example12.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
+@Configuration // this is reuired to resolve $ symbols in @Value annotations, default values injected.
 @PropertySource("person-data-11.properties")
 public class AppConfig {
 
